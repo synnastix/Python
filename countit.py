7bc0b74ddfc6f2e0b6eff1a5712babf46c61cea3
synnastix <33098060+synnastix@users.noreply.github.com>
2020-07-29 11:13:25 -0500

    Create countit.py

---
+++ countit.py
@@ -0,0 +1,28 @@
+# Multi purpose data manipulation assuming an input csv with 'year', 'thing', 'value' columns
+
+import pandas as pd
+
+# Options to set for column width and rows
+
+#pd.set_option('display.max_colwidth', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
+
+# Read the data - encoding optional
+df = pd.read_csv("data.csv", encoding='ISO-8859-1')
+
+# Trim whitespace that may cause issues
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+# Groupby value
+df1 = df.groupby(['year', 'thing']).sum()
+
+# Groupby pivot table
+df2 = df.groupby(['year','thing'],as_index = False).sum().pivot('year','thing').fillna(0)
+
+# Cumulative sum
+df3 = df['cumulativesum'] = df.groupby('thing')['value'].transform(lambda g: g.cumsum())
+df3 = df.set_index('year')
+
+# Output
+print (df3)
