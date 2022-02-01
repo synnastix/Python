be3fdcb383e43870077b82f103f035d44ed9cbec
synnastix <33098060+synnastix@users.noreply.github.com>
2022-01-31 19:45:02 -0600

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,22 +1,29 @@
 import pandas as pd
+import openpyxl
 
 # Specify inputs / outputs
 
-data = 'test.csv' # This is your source data
-wordlist = 'wordlist.csv' # This is your list of terms for searching
-true_out = 'junk.csv' # Rows where a match was found will end up here
-false_out = 'not_junk.csv' # Rows where a match was not found will end up here
-colx = 'col2' # Specify column to search within based on your column headers in your data
-colz = 'key' # Specify column to load based on your column header in your wordlist
+data = 'data.xlsx' # This is your source data
+wordlist = 'wordlist.txt' # This is your list of terms for searching
+true_out = 'true_rows.csv' # Rows where a match was found will end up here
+false_out = 'false_rows.csv' # Rows where a match was not found will end up here
+colx = 'text' # Specify column to search within based on your column headers in your data
+colz = 'key' # Specify column to load based on your column header in your wordlist if using CSV
 
-# Read in csv as dataframe and normalize data to check in lower case
-df = pd.read_csv(data)
+# Read in data as dataframe and normalize data to check in lower case
+# df = pd.read_csv(data)
+df = pd.read_excel(data, engine='openpyxl')
+
 df[colx] = df[colx].str.lower()
 
-# Read in csv as wordlist and normalize in lower case
-list_csv = pd.read_csv(wordlist)
-list = list_csv[colz].tolist()  # Convert column to list
+# Read in csv as wordlist
+#list_csv = pd.read_csv(wordlist)
+#list = list_csv[colz].tolist()  # Convert column to list
 
+# Read text file as wordlist
+list = [line.strip() for line in open(wordlist,'r')]
+
+# Normalize list in lowercase
 for i in range(len(list)):
     list[i] = list[i].lower()
 
